892625d7b3b1335f161e4608c9a46f155b88eb52
condagroup <jake.waang@gmail.com>
2023-11-15 18:55:36 +0200

    Switch card element to payment element of Stripe

--- public/paymentGateways/stripe/stripe.js
+++ public/paymentGateways/stripe/stripe.js
@@ -1,39 +1,60 @@
 if (stripeKey) {
     let stripe = Stripe(stripeKey);
-    let elements = stripe.elements();
+
     let style = {
         base: {
-            fontSize: '16px',
-            color: '#32325d',
-            border: '1px solid red',
+            fontSize: "16px",
+            color: "#32325d",
+            border: "1px solid red",
         },
     };
 
-    let card = elements.create('card', {style: style});
-    card.mount('#card-element');
+    // let card = elements.create('card', {style: style});
+    // card.mount('#card-element');
+
+    // const cardNumber = elements.create('cardNumber');
+    // cardNumber.mount('#card-number');
+
+    // const cardExpiration = elements.create('cardExpiry');
+    // cardExpiration.mount('#card-expiration');
+
+    // const cardCvc = elements.create('cardCvc');
+    // cardCvc.mount('#card-cvc');
+
+    const options = {
+        mode: "setup",
+        currency: "usd",
+        // Fully customizable with appearance API.
+        appearance: {
+            /*...*/
+        },
+    };
+    const elements = stripe.elements(options);
+    const paymentElement = elements.create("payment", {wallets: "applePay"});
+    paymentElement.mount("#payment-element");
 
     function stripe_payment() {
-        $('#payment_method').parent().removeClass('has-error');
-        stripe.createToken(card).then(function (result) {
-            if (result.error) {
-                let errorElement = document.getElementById('card-errors');
-                errorElement.textContent = result.error.message;
-            } else {
-                stripeTokenHandler(result.token);
-            }
-        });
-
+        $("#payment_method").parent().removeClass("has-error");
+        stripe
+            .confirmPayment(elements)
+            .then(function (result) {
+                console.log(result, "123123");
+                if (result.error) {
+                    let errorElement = document.getElementById("card-errors");
+                    errorElement.textContent = result.error.message;
+                } else {
+                    stripeTokenHandler(result.token);
+                }
+            });
     }
 
     function stripeTokenHandler(token) {
-        let form = document.getElementById('paymentForm');
-        let hiddenInput = document.createElement('input');
-        hiddenInput.setAttribute('type', 'hidden');
-        hiddenInput.setAttribute('name', 'stripeToken');
-        hiddenInput.setAttribute('value', token.id);
+        let form = document.getElementById("paymentForm");
+        let hiddenInput = document.createElement("input");
+        hiddenInput.setAttribute("type", "hidden");
+        hiddenInput.setAttribute("name", "stripeToken");
+        hiddenInput.setAttribute("value", token.id);
         form.appendChild(hiddenInput);
         form.submit();
     }
 }
-
-
--- resources/views/paymentGateways/stripe/stripeInput.blade.php
+++ resources/views/paymentGateways/stripe/stripeInput.blade.php
@@ -1,6 +1,30 @@
 <fieldset id="stripe_div" class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6 hidden">
     <div class="w-full">
-        <div id="card-element" class="w-full h-12 rounded-lg px-4 border border-[#D9DBE9] py-3"></div>
+        <div id="payment-element">
+            <!-- Elements will create form elements here -->
+        </div>
+        {{-- <div class="form-group">
+            <label for="card-number">Card Number</label>
+            <div id="card-number" class="w-full h-12 rounded-lg px-4 border border-[#D9DBE9] py-3"></div>
+        </div>
+
+        <div class="input-group">
+            <div class="form-group">
+                <label for="card-expiration">Card Expiration</label>
+                <div id="card-expiration" class="w-full h-12 rounded-lg px-4 border border-[#D9DBE9] py-3"></div>
+            </div>
+
+            <div class="form-group">
+                <label for="card-cvc">Card CVC</label>
+                <div id="card-cvc" class="w-full h-12 rounded-lg px-4 border border-[#D9DBE9] py-3"></div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="postal-code">ZIP or Postal Code</label>
+            <input type="text" class="w-full h-12 rounded-lg px-4 border border-[#D9DBE9] py-3" id="postal-code" name="postal-code" required>
+        </div> --}}
+        {{-- <div id="card-element" class="w-full h-12 rounded-lg px-4 border border-[#D9DBE9] py-3"></div> --}}
         <div id="card-errors" class="text-danger" role="alert"></div>
     </div>
 </fieldset>
